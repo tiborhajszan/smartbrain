13f008608f6248616bb16276e62feee8c3fee9f8
Tibor Hajszan <thajszan@t-online.hu>
2025-04-21 15:52:11 +0200

    Formatted webframe logic code.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// #####################################################################################################################
+// Zero To Mastery Academy
+// Complete Web Developer in 2025: Zero to Mastery
+// Final Project | SmartBrain | Application Webframe Logic
+// #####################################################################################################################
 
-createRoot(document.getElementById('root')).render(
+import {createRoot} from "react-dom/client"
+import {StrictMode} from "react"
+import "./main.css"
+import App from "./App.jsx"
+
+// rendering application webframe ######################################################################################
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
