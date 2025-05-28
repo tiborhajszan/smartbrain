fcc37495b04a8fd49fe91e32fcbc016df9700d52
Tibor Hajszan <thajszan@t-online.hu>
2025-05-28 15:35:37 +0200

    Developed Input Field component.

--- src/components/main/InputField.jsx
+++ src/components/main/InputField.jsx
@@ -1,8 +1,8 @@
-// #####################################################################################################################
-// Zero To Mastery Academy
-// Complete Web Developer in 2025: Zero to Mastery
-// Final Project | SmartBrain | Input Field Component Logic
-// #####################################################################################################################
+//######################################################################################################################
+//  Zero To Mastery Academy
+//  Complete Web Developer in 2025: Zero to Mastery
+//  Final Project | SmartBrain | Input Field Component Logic
+//######################################################################################################################
 
 import React from "react";
 import "./InputField.css";
@@ -12,9 +12,8 @@ import "./InputField.css";
 function InputField() {
   return (
     <div className="input-field">
-      <p>
-        Enter an image URL below and click "Detect". Our AI agent will find and mark human faces in the image.
-      </p>
+      <h1>SmartBrain Face Detector</h1>
+      <p>Enter an image URL below and click "Detect". SmartBrain will find and mark human faces in the image.</p>
       <div>
         <input id="image-url" type="text" placeholder="Enter Image URL" />
         <button id="detect-btn">Detect</button>
