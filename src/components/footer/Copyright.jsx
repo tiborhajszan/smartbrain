99ce1902fc527466a4021d9cc9202c9776d64140
Tibor Hajszan <thajszan@t-online.hu>
2025-06-16 13:13:58 +0200

    Built the Footer > Copyright component.

--- src/components/footer/Copyright.jsx
+++ src/components/footer/Copyright.jsx
@@ -1,5 +1,4 @@
 //######################################################################################################################
-//  Zero To Mastery Academy
 //  Complete Web Developer in 2025: Zero to Mastery
 //  Final Project | SmartBrain | Copyright Component Logic
 //######################################################################################################################
@@ -11,7 +10,13 @@ import "./Copyright.css";
 export default function Copyright() {
   return (
     <div className="copyright">
-      Copyright
+      <a href="https://www.linkedin.com/in/tibor-hajszan/" target="_blank">
+        &copy; 2025 Tibor Hajszan&nbsp;&nbsp;<i className="fa fa-arrow-right"></i>
+      </a>
+      <p>SmartBrain is the guided final project of ZtM course:</p>
+      <a href="https://zerotomastery.io/courses/coding-bootcamp/" target="_blank">
+        Complete Web Developer in 2025 : Zero to Mastery&nbsp;&nbsp;<i className="fa fa-arrow-right"></i>
+      </a>
     </div>
   );
 };
