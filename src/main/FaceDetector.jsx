34dea4017ec7b7d2c4ffc4db2304784c330df205
Tibor Hajszan <thajszan@t-online.hu>
2025-07-03 16:46:59 +0200

    FaceDetector: under development...

--- src/main/FaceDetector.jsx
+++ src/main/FaceDetector.jsx
@@ -3,7 +3,7 @@
 //  Final Project | SmartBrain | Face Detector Component Logic
 //######################################################################################################################
 
-import {useRef, useState} from "react";
+import {useRef, useState, useEffect} from "react";
 import InputField from "./InputField.jsx";
 import aiFace from "/src/assets/face.jpg";
 import "./FaceDetector.css";
@@ -17,23 +17,53 @@ export default function FaceDetector() {
   const inputRef = useRef(null);
   const [imageUrl, setImageUrl] = useState(aiFace);
   const imageRef = useRef(null);
+  const scrollFlag = useRef(false);
 
-  // detect onclick callback -------------------------------------------------------------------------------------------
+  // press enter callback ----------------------------------------------------------------------------------------------
+
+  function onEnter(event) {
+    if (event.keyCode === 13) onDetect();
+    return;
+  };
+
+  // click detect callback ---------------------------------------------------------------------------------------------
 
   function onDetect() {
+    if (inputRef.current.value === "" && imageUrl === aiFace) return;
     if (inputRef.current.value === imageUrl) return;
+    // const supportedExtensions = [".jpeg", ".jpg", ".png", ".tiff", ".tif", ".bmp", ".webp"];
+    // if (!supportedExtensions.some(ext => inputRef.current.value.endsWith(ext))) return;
     if (inputRef.current.value === "") setImageUrl(aiFace);
     else setImageUrl(inputRef.current.value);
-    window.scrollTo({top: imageRef.current.offsetTop - 158, behavior: "smooth"});
+    scrollFlag.current = true;
+    return;
   };
+
+  // image error callback ----------------------------------------------------------------------------------------------
+
+  function onImageError() {
+    inputRef.current.value = "Image Error!";
+    setImageUrl(aiFace);
+    return;
+  };
+
+  // image handler -----------------------------------------------------------------------------------------------------
+
+  useEffect(() => {
+    if (scrollFlag.current) {
+      window.scrollTo({top: imageRef.current.offsetTop - 158, behavior: "smooth"});
+      scrollFlag.current = false;
+    };
+    return;
+  }, [imageUrl]);
 
   // rendering component -----------------------------------------------------------------------------------------------
 
   return (
     <main>
       <p>Enter an image URL and click "Detect". SmartBrain will find and mark human faces in your image.</p>
-      <InputField refInput={inputRef} callDetect={onDetect} />
-      <img ref={imageRef} src={imageUrl} alt="Analyzed Image" />
+      <InputField refInput={inputRef} pressEnter={onEnter} clickDetect={onDetect} />
+      <img ref={imageRef} src={imageUrl} alt="Analyzed Image" onError={onImageError} />
     </main>
   );
 };
