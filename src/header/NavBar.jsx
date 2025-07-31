e9a4edc72b87f8dfb787f0e84bbce78ea0223630
Tibor Hajszan <thajszan@t-online.hu>
2025-07-31 16:37:36 +0200

    Navigation Bar : under construction...

--- src/header/NavBar.jsx
+++ src/header/NavBar.jsx
@@ -12,37 +12,44 @@ import "./NavBar.css";
 
 export default function NavBar(props) {
 
-  // reference and state -----------------------------------------------------------------------------------------------
+  // ref and state -----------------------------------------------------------------------------------------------------
 
-  const refSpan = useRef(null);
-  const refProfile = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleProfile = () => setIsOpen(!isOpen);
+  const spanRef = useRef(null);
+  const profileRef = useRef(null);
+  const [profileOpen, setProfileOpen] = useState(false);
 
-  // effect ------------------------------------------------------------------------------------------------------------
+  // toggle profile callback -------------------------------------------------------------------------------------------
+
+  function toggleProfile() {
+    setProfileOpen(!profileOpen);
+  };
+
+  // close profile effect ----------------------------------------------------------------------------------------------
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (!refSpan.current.contains(event.target)
-      && !refProfile.current.contains(event.target)) {
-        setIsOpen(false);
+      if (!spanRef.current.contains(event.target)
+      && !profileRef.current.contains(event.target)) {
+        setProfileOpen(false);
       };
     };
     document.addEventListener("mousedown", handleClickOutside);
     return () => {document.removeEventListener("mousedown", handleClickOutside);};
   });
 
-  // rendering ---------------------------------------------------------------------------------------------------------
+  // rendering component -----------------------------------------------------------------------------------------------
 
   return (
     <nav ref={props.refNav}>
-      <Logo />
-      <a href="#" target="_self"><i className="fa fa-sign-out fa-flip-horizontal"></i>&nbsp;&nbsp;Log Out</a>
-      <span ref={refSpan} onClick={toggleProfile}><i className="fa fa-user"></i></span>
-      <Profile
-        ref={refProfile} profileOpen={isOpen}
-        userName={"Tibor Hajszan"} detectCount={"#110"} lastLogin={"2025.06.23"}
-      />
+      <div>
+        <Logo />
+        <span className="logout"><i className="fa fa-sign-out fa-flip-horizontal"></i>&nbsp;&nbsp;Log Out</span>
+        <span ref={spanRef} className="user" onClick={toggleProfile}><i className="fa fa-user"></i></span>
+        <Profile
+          ref={profileRef} profileOpen={profileOpen}
+          userName={"Tibor Hajszan"} detectCount={"#110"} lastLogin={"2025.06.23"}
+        />
+      </div>
     </nav>
   );
 };
