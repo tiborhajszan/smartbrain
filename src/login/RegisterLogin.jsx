590fe683c46b5ed2c30fae14b55a01915ade62e1
Tibor Hajszan <thajszan@t-online.hu>
2025-08-15 15:30:10 +0200

    Register / Login : finalized.

--- src/login/RegisterLogin.jsx
+++ src/login/RegisterLogin.jsx
@@ -5,19 +5,18 @@
 
 import "./RegisterLogin.css";
 
-// input field component renderer ######################################################################################
+// register/login component renderer ###################################################################################
 
 export default function RegisterLogin(props) {
   return (
     <form action="">
       <legend>{props.route}</legend>
-      {props.route === "Register"
-        ? <><label htmlFor="name">Name:</label><input type="text" name="username" /></>
-        : null}
+      {props.route === "Register" ? <label htmlFor="name">Name:</label> : null}
+      {props.route === "Register" ? <input type="text" name="username" required /> : null}
       <label htmlFor="email">Email:</label>
-      <input type="email" name="email" />
+      <input type="email" name="email" required />
       <label htmlFor="password">Password:</label>
-      <input type="password" name="password" />
+      <input type="password" name="password" required />
       <button type="submit">{props.route}</button>
     </form>
   );
