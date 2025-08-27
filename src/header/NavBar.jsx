2e222bd0ccfd7104a61bbd9cef4f8faf76e5ece3
Tibor Hajszan <thajszan@t-online.hu>
2025-08-27 12:14:25 +0200

    Navigation Bar : updated routing code.

--- src/header/NavBar.jsx
+++ src/header/NavBar.jsx
@@ -18,13 +18,6 @@ export default function NavBar(props) {
   const profileRef = useRef(null);
   const [profileOpen, setProfileOpen] = useState(false);
 
-  // toggle profile callback -------------------------------------------------------------------------------------------
-
-  function toggleProfile() {
-    setProfileOpen(!profileOpen);
-    return;
-  };
-
   // click outside callback --------------------------------------------------------------------------------------------
   
   function clickOutside(event) {
@@ -40,41 +33,54 @@ export default function NavBar(props) {
     return () => document.removeEventListener("mousedown", clickOutside);
   });
 
-  // rendering component -----------------------------------------------------------------------------------------------
+  // rendering detector page navigation bar ----------------------------------------------------------------------------
 
-  return (
+  if (props.route === "Detector") return (
     <nav ref={props.refNav}>
       <div>
-        {props.route === "Login" || props.route === "Register"
-        ? <span>SmartBrain</span>
-        : <span onClick={() => props.setRoute("Login")}>
-            <i className="fa fa-sign-out fa-flip-horizontal"></i>&nbsp;&nbsp;Logout
-          </span>}
+        <span onClick={() => props.setRoute("Login")}>
+          <i className="fa fa-sign-out fa-flip-horizontal"></i>&nbsp;&nbsp;Logout
+        </span>
         <Logo />
-        {props.route === "Login"
-        ? <span onClick={() => props.setRoute("Register")}>
-            <i className="fa fa-edit"></i>&nbsp;&nbsp;Register
-          </span>
-        : null}
-        {props.route === "Register"
-        ? <span onClick={() => props.setRoute("Login")}>
-            <i className="fa fa-sign-in"></i>&nbsp;&nbsp;Login
-          </span>
-        : null}
-        {props.route === "Detector"
-        ? <span ref={userRef} onClick={toggleProfile}>
-            <i className="fa fa-user"></i>&nbsp;&nbsp;Profile
-          </span>
-        : null}
-        {props.route === "Detector"
-        ? <Profile
-            refProfile={profileRef} profileOpen={profileOpen}
-            userName={"Tibor Hajszan"}
-            detectCount={"#110"}
-            lastLogin={"2025.06.23"}
-          />
-        : null}
+        <span ref={userRef} onClick={() => setProfileOpen(!profileOpen)}>
+          <i className="fa fa-user"></i>&nbsp;&nbsp;Profile
+        </span>
+        <Profile
+          refProfile={profileRef} profileOpen={profileOpen}
+          userName={"Tibor Hajszan"}
+          detectCount={"#110"}
+          lastLogin={"2025.06.23"}
+        />
+      </div>
+    </nav>
+  )
+
+  // rendering register page navigation bar ----------------------------------------------------------------------------
+
+  else if (props.route === "Register") return (
+    <nav ref={props.refNav}>
+      <div>
+        <span>SmartBrain</span>
+        <Logo />
+        <span onClick={() => props.setRoute("Login")}>
+          <i className="fa fa-sign-in"></i>&nbsp;&nbsp;Login
+        </span>
+      </div>
+    </nav>
+  )
+
+  // rendering login page navigation bar -------------------------------------------------------------------------------
+
+  else return (
+    <nav ref={props.refNav}>
+      <div>
+        <span>SmartBrain</span>
+        <Logo />
+        <span onClick={() => props.setRoute("Register")}>
+          <i className="fa fa-edit"></i>&nbsp;&nbsp;Register
+        </span>
       </div>
     </nav>
   );
+
 };
