541ed0ba2d40bdc92b36cd0d063dfcfe13d04e17
Tibor Hajszan <thajszan@t-online.hu>
2025-09-11 16:14:07 +0200

    Register / Login : integrating API.

--- src/login/RegisterLogin.jsx
+++ src/login/RegisterLogin.jsx
@@ -3,21 +3,62 @@
 //  Final Project | SmartBrain | Register/Login Component Logic
 //######################################################################################################################
 
+import {useRef} from "react";
 import "./RegisterLogin.css";
 
 // register/login component renderer ###################################################################################
 
 export default function RegisterLogin(props) {
+
+  // ref ---------------------------------------------------------------------------------------------------------------
+
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  // click login callback ----------------------------------------------------------------------------------------------
+
+  function clickLogin() {
+
+    let fetchUrl = "http://localhost:3000/login";
+
+    const payLoad = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    };
+
+    if (props.route === "Register") {
+      fetchUrl = "http://localhost:3000/register";
+      payLoad.name = nameRef.current.value;
+    };
+
+    fetch(fetchUrl, {
+      method: "post",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(payLoad)
+    })
+    .then(response => {
+      console.log(response.text());
+    })
+    .catch(error => {
+      console.log("Catch: ", error);
+    });
+
+    return;
+  };
+
+  // rendering component -----------------------------------------------------------------------------------------------
+
   return (
     <div className="register-login">
       <legend>{props.route}</legend>
       {props.route === "Register" ? <p>Name:</p> : null}
-      {props.route === "Register" ? <input type="text" /> : null}
+      {props.route === "Register" ? <input ref={nameRef} type="text" /> : null}
       <p>Email:</p>
-      <input type="email" />
+      <input ref={emailRef} type="email" />
       <p>Password:</p>
-      <input type="password" />
-      <button onClick={() => props.setRoute("Detector")}>{props.route}</button>
+      <input ref={passwordRef} type="password" />
+      <button onClick={clickLogin}>{props.route}</button>
     </div>
   );
 };
